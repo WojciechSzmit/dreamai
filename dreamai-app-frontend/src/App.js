a28cbc09c515a4e5688641d0b368b93bb09e9c02
Wojciech Szmit <wojs4k@gmail.com>
2024-08-30 06:50:47 +0200

    RWD

--- dreamai-app-frontend/src/App.js
+++ dreamai-app-frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './App.css'; // Import the CSS file
+import './App.css'; 
 
 function App() {
   const [prompt, setPrompt] = useState('');
@@ -15,7 +15,7 @@ function App() {
         prompt,
         enable_safety_checker: enableSafetyChecker,
       });
-      setResponse(res.data); // Store the response object
+      setResponse(res.data); 
     } catch (error) {
       console.error('Error fetching completion', error);
     }
@@ -23,12 +23,12 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Completion Generator</h1>
+      <h1>Dream machine</h1>
       <form onSubmit={handleSubmit}>
         <textarea
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
-          placeholder="Enter your prompt here"
+          placeholder="Enter your dream here"
         />
         <label>
           <input
@@ -38,7 +38,7 @@ function App() {
           />
           Enable Safety Checker
         </label>
-        <button type="submit">Generate</button>
+        <button type="submit">Start dreaming</button>
       </form>
       <div className="response-container">
         <h2>Response:</h2>
