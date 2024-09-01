68a50ad2a1326bee747a252f1e7ee28902f37e0e
Wojciech Szmit <wojs4k@gmail.com>
2024-09-01 20:52:36 +0200

    render link5

--- dreamai-app-frontend/src/App.js
+++ dreamai-app-frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
     e.preventDefault();
 
     try {
-      const res = await axios.post('http://localhost:5000/api/completion', {
+      const res = await axios.post('https://dreamai-79vb.onrender.com/', {
         prompt,
         enable_safety_checker: enableSafetyChecker,
       });
