61e4b5941d35f3244cced3fa851f70d600bf79a5
Wojciech Szmit <wojs4k@gmail.com>
2024-09-01 19:05:49 +0200

    path4

--- dreamai-app-backend/server.js
+++ dreamai-app-backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const modelRoutes = require('./routes/modelRoutes');
 const cors = require('cors');
-const path = require('path');
+//const path = require('path');
 
 
 
@@ -19,11 +19,11 @@ app.use(express.json());
 app.use('/api', modelRoutes);
 
 
-app.use(express.static(path.join(__dirname, 'dreamai-app-frontend/build')));
+/* app.use(express.static(path.join(__dirname, 'dreamai-app-frontend/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dreamai-app-frontend/src', 'index.js'));
-});
+}); */
 
 
 app.listen(PORT, () => {
