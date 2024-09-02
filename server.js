d500caa1f7022872f9442d3eb6274beebfcfb490
Wojciech Szmit <wojs4k@gmail.com>
2024-09-02 17:07:24 +0200

    path2

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.use('/api', modelRoutes);
 
 
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+/* app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+}); */
 
 
 
