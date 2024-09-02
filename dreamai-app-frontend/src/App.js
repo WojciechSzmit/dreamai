80bee5ffd62e4f6eb9935407ea5ea1ae06ad329a
Wojciech Szmit <wojs4k@gmail.com>
2024-09-02 06:28:55 +0200

    env remove

--- dreamai-app-frontend/src/App.js
+++ dreamai-app-frontend/src/App.js
@@ -12,7 +12,9 @@ function App() {
     e.preventDefault();
 
     try {
-      const res = await axios.post('https://dreamai-79vb.onrender.com/api/completion', {
+      const res = await axios.post('http://localhost:5000/api/completion', {
+      //  const res = await axios.post('https://dreamai-79vb.onrender.com/api/completion', {
+        
         prompt,
         enable_safety_checker: enableSafetyChecker,
       });
