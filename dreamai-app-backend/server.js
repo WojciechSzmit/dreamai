77adcf0bad7b01030ae035d64b613c23d6876de4
Wojciech Szmit <wojs4k@gmail.com>
2024-09-02 08:52:34 +0200

    path88899

--- dreamai-app-backend/server.js
+++ dreamai-app-backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const modelRoutes = require('./routes/modelRoutes');
 const cors = require('cors');
-//const path = require('path');
+const path = require('path');
 
 
 
